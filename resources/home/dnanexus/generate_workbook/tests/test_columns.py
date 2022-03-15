9bee5bc6c01e692aa9984e0beec32c4ed61faa05
jethro <rainford1995@gmail.com>
2022-03-15 13:16:32 +0000

    better tests for format columns

--- resources/home/dnanexus/generate_workbook/tests/test_columns.py
+++ resources/home/dnanexus/generate_workbook/tests/test_columns.py
@@ -1,4 +1,5 @@
 import argparse
+from itertools import chain
 import os
 from pathlib import Path
 import subprocess
@@ -6,6 +7,7 @@ import sys
 
 import numpy as np
 import pandas as pd
+from parso import split_lines
 
 import pytest
 
@@ -31,56 +33,13 @@ def read_test_vcf(vcf_file):
         filter=None, include=None, keep=False, merge=False,
         out_dir='',
         output='',
-        panel='', print_columns=False, reads='', rename=None, reorder=None,
+        panel='', print_columns=False, print_header=False, reads='', rename=None, reorder=None,
         sample='', sheets=['variants'], summary=None, usable_reads='',
         vcfs=[columns_vcf], workflow=('', '')
     ))
-    vcf_df, csq_fields = vcf_handler.read(columns_vcf)
+    vcf_df = vcf_handler.read(columns_vcf)
 
-    return vcf_df, csq_fields
-
-
-def read_csq_from_vcf() -> list:
-    """
-    Reads CSQ strings direct from VCF to compare against VCF using
-    subprocess with grep, cut and sed
-
-    Returns
-    -------
-    csq_strings : list
-        list of each variants/transcript csq string
-    """
-    # read in INFO column from VCF to list of records, split the multiple
-    # transcripts to separate lines by splitting with sed on commas
-    info_strings = subprocess.run((
-        f"grep -v '^#' {TEST_DATA_DIR}/multi_transcript_annotation.vcf | "
-        "cut -f8 | sed 's/,/\\n/g'")
-        , shell=True, capture_output=True).stdout.decode().rstrip('\n').split('\n')
-
-    # retain just CSQ part, results in one item with all CSQ joined by '|'
-    csq_strings = [x.split('CSQ=')[-1] for x in info_strings]
-
-    return csq_strings
-
-
-def read_sample_from_vcf() -> list:
-    """
-    Reads values from SAMPLE column direct from VCF to compare against VCF
-    using subprocess with grep and cut
-
-    Returns
-    -------
-    csq_strings : list
-        list of each variants/transcript csq string
-    """
-    # read in INFO column from VCF to list of records, split the multiple
-    # transcripts to separate lines by splitting with sed on commas
-    sample_strings = subprocess.run((
-        f"grep -v '^#' {TEST_DATA_DIR}/multi_transcript_annotation.vcf | "
-        "cut -f10")
-        , shell=True, capture_output=True).stdout.decode().rstrip('\n').split('\n')
-
-    return sample_strings
+    return vcf_df
 
 
 class TestInfoColumn():
@@ -122,9 +81,11 @@ class TestInfoColumn():
 
     ['STR', 'POP_AF', 'RPA', 'TLOD', 'P_GERMLINE', 'ECNT', 'DP', 'RU']
     """
+    test_vcf = "NA12878_unittest.split.vcf"
+
     # run dataframe through splitColumns.info() to split out INFO column
-    vcf_df, _ = read_test_vcf(vcf_file="column_methods_test.vcf")
-    vcf_df = splitColumns.info(vcf_df)
+    vcf_df = read_test_vcf(vcf_file=test_vcf)
+    vcf_df = splitColumns().split(vcf_df)
 
 
     def test_parsed_correct_columns_from_info_records(self) -> None:
@@ -132,261 +93,52 @@ class TestInfoColumn():
         Test that all unique INFO values from all rows correctly parsed and
         set to columns in returned dataframe
         """
-        correct_columns = [
-            'CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'FORMAT',
-            'SAMPLE', 'STR', 'POP_AF', 'RPA', 'TLOD', 'P_GERMLINE',
-            'ECNT', 'DP', 'RU'
-        ]
+        # read all the INFO keys direct from vcf
+        output = subprocess.run(
+            (
+                f"cut -f8 {TEST_DATA_DIR}/{self.test_vcf} | grep -oh "
+                f"';[A-Za-z0-9\_\-\.]*=' | sort | uniq",
+            ), shell=True, capture_output=True
+        )
 
-        assert sorted(correct_columns) == sorted(self.vcf_df.columns.tolist()), (
-            "Number of columns after spliting INFO is incorrect"
+        # get cleaned list that should be df column names
+        stdout = output.stdout.decode().splitlines()
+        stdout = [x.replace(';', '').replace('=', '') for x in stdout]
+
+        assert all([x in self.vcf_df.columns.tolist() for x in stdout]), (
+            'Not all INFO keys parsed to column names'
         )
 
 
-    def test_parsed_correct_DP_values(self):
+    def test_parsed_correct_gnomAD_AF_values(self):
         """
         Test values read into dataframe for DP match the values
         above from the VCF
         """
-        correct_dp_values = [
-            '1400', '1400', '1400', '1563', '2714', '2965',
-            '3077','2943', '2917', '1907', '1892', '2358'
-        ]
-
-        assert correct_dp_values == self.vcf_df['DP'].tolist()
-
-
-    def test_parsed_correct_ECNT_values(self):
-        """
-        Test values read into dataframe for ECNT match the values
-        above from the VCF
-        """
-        correct_ecnt_values = [
-            '5', '5', '5', '2', '1', '2', '2', '2', '1', '1', '1', '1'
-        ]
-
-        assert correct_ecnt_values == self.vcf_df['ECNT'].tolist()
-
-
-    def test_parsed_correct_pop_af_values(self):
-        """
-        Test values read into dataframe for POP_AF match the values
-        above from the VCF
-        """
-        correct_pop_af_values = [
-            '0.001', '0.001', '0.001', '0.001', '0.001', '0.001',
-            '0.001', '0.001', '0.001', '0.001', '0.001', '0.001',
-        ]
-
-        assert correct_pop_af_values == self.vcf_df['POP_AF'].tolist()
-
-
-    def test_parsed_correct_p_germline_values(self):
-        """
-        Test values read into dataframe for P_GERMLINE match the values
-        above from the VCF
-        """
-        correct_p_germline_values = [
-            '-0', '-0', '-0', '-0.029', '-0', '-0',
-            '-0.001', '-0.002', '-0', '-0', '-0', '-0'
-        ]
-
-        assert correct_p_germline_values == self.vcf_df['P_GERMLINE'].tolist()
-
-
-    def test_parsed_correct_rpa_values(self):
-        """
-        Test values read into dataframe for RPA match the values
-        above from the VCF
-        """
-        correct_rpa_values = [
-            '11,10', '11,12', '11,13', '3,2', np.nan, '5,4',
-            np.nan, np.nan, '4,3', np.nan, '8,9', np.nan
-        ]
-
-
-        assert correct_rpa_values == self.vcf_df['RPA'].tolist()
-
-
-    def test_parsed_correct_ru_values(self):
-        """
-        Test values read into dataframe for RU match the values
-        above from the VCF
-        """
-        correct_ru_values = [
-            'A', 'A', 'A', 'CGGCGC', np.nan, 'GTG', np.nan,
-            np.nan, 'CAC', np.nan, 'T', np.nan
-        ]
-
-        assert correct_ru_values == self.vcf_df['RU'].tolist()
-
-
-    def test_parsed_correct_str_values(self):
-        """
-        Test values read into dataframe for STR match the values
-        above from the VCF, since these are just flags and not key=value
-        pairs, these get added to the DF as True if present
-        """
-        correct_str_values = [
-            True, True, True, True, np.nan, True,
-            np.nan, np.nan, True, np.nan, True, np.nan
-        ]
-
-        assert correct_str_values == self.vcf_df['STR'].tolist()
-
-
-    def test_parsed_correct_tlod_values(self):
-        """
-        Test values read into dataframe for TLOD match the values
-        above from the VCF
-        """
-        correct_tlod_values = [
-            '36.38', '418.64', '21.15', '3.87', '3878.54', '87.52',
-            '5.47', '5.02', '36.68', '2904.48', '19.45', '74.01'
-        ]
-
-        assert correct_tlod_values == self.vcf_df['TLOD'].tolist()
-
-
-    def test_info_column_dropped(self):
-        """
-        Tests if INFO column dropped from dataframe after it is expanded to
-        separate columns
-        """
-        assert "INFO" not in self.vcf_df.columns.tolist(), 'INFO column not dropped'
-
-
-class TestCSQ():
-    """
-    Tests for splitColumns.csq that splits out CSQ string to separate
-    columns as defined from header. CSQ fields to split out from test vcf:
-
-    ['SYMBOL', 'VARIANT_CLASS', 'Consequence', 'EXON', 'HGVSc', 'HGVSp',
-    'gnomAD_AF', 'gnomADg_AF', 'CADD_PHRED', 'Existing_variation', 'ClinVar',
-    'ClinVar_CLNDN', 'ClinVar_CLNSIG', 'COSMIC', 'Feature']
-
-    Data transforms as:
-
-    5 variants with multiple comma separated CSQ annotation as:
-
-    CSQ=LEPR|SNV|intron_variant||NM_001003679.3:c.370+16G>T||0.9999|...
-    CSQ=LEPR|SNV|missense_variant|6/20|NM_001003679.3:c.668A>G|...
-    CSQ=LEPR|SNV|intron_variant||NM_001003679.3:c.850-50A>C||0.4683|...
-    CSQ=LEPR|SNV|synonymous_variant|9/20|NM_001003679.3:c.1029T>C|...
-    CSQ=CEP170|SNV|upstream_gene_variant||||0.5003|4.16591e-01|16.36|...
-
-
-    Multiple transcripts split to separate rows if present:
-
-    LEPR|SNV|intron_variant||NM_001003679.3:c.370+16G>T||...
-    LEPR|SNV|intron_variant||NM_001003680.3:c.370+16G>T||...
-    LEPR|SNV|intron_variant||NM_001198687.2:c.370+16G>T||...
-    LEPR|SNV|intron_variant||NM_001198688.1:c.370+16G>T||...
-    LEPR|SNV|intron_variant||NM_001198689.2:c.370+16G>T||...2
-    LEPR|SNV|intron_variant||NM_002303.6:c.370+16G>T||...
-
-
-    LEPR|SNV|missense_variant|6/20|NM_001003679.3:c.668A>G|...
-    LEPR|SNV|missense_variant|6/20|NM_001003680.3:c.668A>G|...
-    LEPR|SNV|missense_variant|5/19|NM_001198687.2:c.668A>G|...
-    LEPR|SNV|missense_variant|5/19|NM_001198688.1:c.668A>G|...
-    LEPR|SNV|missense_variant|5/19|NM_001198689.2:c.668A>G|.
-    LEPR|SNV|missense_variant|6/20|NM_002303.6:c.668A>G|...
-
-
-    LEPR|SNV|intron_variant||NM_001003679.3:c.850-50A>C||0.4683|...
-    LEPR|SNV|intron_variant||NM_001003680.3:c.850-50A>C||0.4683|...
-    LEPR|SNV|intron_variant||NM_001198687.2:c.850-50A>C||0.4683|...
-    LEPR|SNV|intron_variant||NM_001198688.1:c.850-50A>C||0.4683|...
-    LEPR|SNV|intron_variant||NM_001198689.2:c.850-50A>C||0.4683|...
-    LEPR|SNV|intron_variant||NM_002303.6:c.850-50A>C||0.4683|...
-
-
-    LEPR|SNV|synonymous_variant|9/20|NM_001003679.3:c.1029T>C|...
-    LEPR|SNV|synonymous_variant|9/20|NM_001003680.3:c.1029T>C|...
-    LEPR|SNV|synonymous_variant|8/19|NM_001198687.2:c.1029T>C|...
-    LEPR|SNV|synonymous_variant|8/19|NM_001198688.1:c.1029T>C|...
-    LEPR|SNV|synonymous_variant|8/19|NM_001198689.2:c.1029T>C|...
-    LEPR|SNV|synonymous_variant|9/20|NM_002303.6:c.1029T>C|...
-
-
-    CEP170|SNV|upstream_gene_variant||||0.5003|4.16591e-01|16.36|...
-    CEP170|SNV|upstream_gene_variant||||0.5003|4.16591e-01|16.36|...
-    SDCCAG8|SNV|5_prime_UTR_variant|1/20|NM_001350246.2:c.-1159T>A||...
-    SDCCAG8|SNV|5_prime_UTR_variant|1/19|NM_001350247.2:c.-1047T>A||...
-    SDCCAG8|SNV|5_prime_UTR_variant|1/19|NM_001350248.2:c.-47T>A||...
-    SDCCAG8|SNV|5_prime_UTR_variant|1/18|NM_001350249.2:c.-354T>A||...
-    SDCCAG8|SNV|5_prime_UTR_variant|1/21|NM_001350251.2:c.-1420T>A||...
-    SDCCAG8|SNV|5_prime_UTR_variant|1/18|NM_006642.5:c.-47T>A||0.5003|...
-    CEP170|SNV|upstream_gene_variant||||0.5003|4.16591e-01|16.36|...
-
-
-    Example split out annotation for one variant / transcript:
-
-    SYMBOL | VARIANT_CLASS | Consequence    | EXON | HGVSc \
-    ------------------------------------------------------
-    LEPR   | SNV           | intron_variant |      | NM_001003679.3:c.850-50A>C \
-
-    HGVSp | gnomAD_AF | gnomADg_AF  | CADD_PHRED | Existing_variation \
-    -----------------------------------------------------------------
-          | 0.4683    | 4.99514e-01 | 7.575      | rs3762273 \
-
-    ClinVar | ClinVar_CLNDN | ClinVar_CLNSIG | COSMIC | Feature
-    -----------------------------------------------------------
-    1263741 | not_provided  | Benign         |        | NM_001003679.3
-
-    """
-    # read in test vcf with multiple transcript of annotation for each variant
-    vcf_df, csq_fields = read_test_vcf(vcf_file="multi_transcript_annotation.vcf")
-
-    # count total number of transcript annotation across all variants
-    # number of transcripts will equal number of ',' + 1 for each string
-    transcript_count = 0
-    tmp_df = pd.DataFrame()
-    tmp_df['CSQ'] = vcf_df['INFO'].apply(lambda x: x.split('CSQ=')[-1])
-
-    for idx, row in tmp_df.iterrows():
-        transcript_count += row['CSQ'].count(',') + 1
-
-    # call splitColumns.csq() method to split out CSQ column to test against
-    split_csq_vcf_df, expanded_vcf_rows = splitColumns.csq(vcf_df, csq_fields)
-
-    # read csq strings direct from vcf to compare against dataframe columns
-    csq_strings = read_csq_from_vcf()
-
-
-    def test_correct_csq(self):
-        """
-        Test the columns in the dataframe of expanded CSQ match what is read in
-        from file
-        """
-        # dump out all csq columns from dataframe as a '|' joined list
-        df_csq_values = self.split_csq_vcf_df[self.csq_fields].agg('|'.join, axis=1).tolist()
-
-        assert df_csq_values == self.csq_strings, (
-            "CSQ values from dataframe do not match what is in file"
+        # read gnomAD_AF values from vcf
+        output = subprocess.run(
+            (
+                f"grep -v '^#' {TEST_DATA_DIR}/{self.test_vcf} | grep -oh "
+                f"'gnomAD_AF=[0-9\.e\-]*;' | sort | uniq"
+            ), shell=True, capture_output=True
         )
 
+        # clean up values
+        stdout = output.stdout.decode().splitlines()
+        stdout = sorted(list([
+            x.replace(';', '').replace('gnomAD_AF=', '') for x in stdout
+        ]))
 
-    def test_correct_total_number_rows_after_csq_explode(self):
-        """
-        Test that when we call .explode() on CSQ column to split out multiple
-        transcripts to separate rows that this results in the correct number
-        of rows
-        """
-        assert len(self.split_csq_vcf_df.index) == self.transcript_count, (
-            "Differing total of rows after .explode() on CSQ column"
+        # get AF values from dataframe
+        df_values = sorted(list(self.vcf_df['CSQ_gnomAD_AF'].unique().tolist()))
+
+        assert set(stdout) - set(df_values) == set(), (
+            "gnomAD AF values present in VCF not in dataframe"
         )
 
-
-    def test_all_csq_fields_present_in_dataframe(self):
-        """
-        Tests that the csq fields parsed from header are all present in the
-        resultant dataframe
-        """
-        assert all(map(
-            lambda v: v in self.split_csq_vcf_df.columns.tolist(), self.csq_fields
-        )), "CSQ columns parsed from header not present in df after splitColumns.csq()"
+        assert set(df_values) - set(stdout) == set(), (
+            "gnomAD AF values present in dataframe not in VCF"
+        )
 
 
 class TestFormatSample():
@@ -394,24 +146,46 @@ class TestFormatSample():
     Tests for splitColumns.format_fields() that creates new columns from FORMAT
     fields, combining with respective values from SAMPLE column
     """
-    # run dataframe through splitColumns.format_fields() to split out FORMAT/
-    # SAMPLE columns
-    vcf_df, _ = read_test_vcf(vcf_file="multi_transcript_annotation.vcf")
-    vcf_df = splitColumns.format_fields(vcf_df)
+    # run dataframe through splitColumns.format_fields() to split out FORMAT/SAMPLE
+    test_vcf = "NA12878_unittest.split.vcf"
 
-    # turn columns of format sample values back to ':' joined strings as in vcf
-    format_columns = ['GT', 'AD', 'DP', 'GQ', 'PL']
-    sample_strings = vcf_df[format_columns].agg(':'.join, axis=1).tolist()
+    # run dataframe through splitColumns.info() to split out INFO column
+    vcf_df = read_test_vcf(vcf_file=test_vcf)
+    vcf_df = splitColumns().split(vcf_df)
 
-    # read sample strings from test vcf directly
-    vcf_sample_strings = read_sample_from_vcf()
+
+    # get list of FORMAT fields from VCF FORMAT column
+    output = subprocess.run((
+        f"grep -v '^#' {TEST_DATA_DIR}/{test_vcf} | cut -f9 | sort | uniq"
+    ), shell=True, capture_output=True)
+
+    format_fields = sorted(output.stdout.decode().split())[0].split(':')
+
+    # fields already present in df from INFO column will have had suffix added,
+    # therefore we will check if they are there to select the correct fields
+    for idx, field in enumerate(format_fields):
+        if f"{field} (FMT)" in vcf_df.columns:
+            format_fields[idx] = f"{field} (FMT)"
+
+    # get all SAMPLE values from vcf
+    output = subprocess.run((
+        f"grep -v '^#' {TEST_DATA_DIR}/{test_vcf} | cut -f10"
+    ), shell=True, capture_output=True)
+
+    sample_strings_vcf = output.stdout.decode().splitlines()
+
+    # read the sample strings for columns in df, join back together and remove
+    # nans from columns that have no values
+    sample_strings_df = vcf_df[format_fields].astype(str)
+    sample_strings_df = sample_strings_df.agg(':'.join, axis=1).tolist()
+    sample_strings_df = [x.replace(':nan', '') for x in sample_strings_df]
 
 
     def test_format_sample_values_are_correct(self):
         """
         Tests if the FORMAT / SAMPLE values in dataframe match values in vcf
         """
-        assert self.sample_strings == self.vcf_sample_strings, (
+        assert self.sample_strings_df == self.sample_strings_vcf, (
             "SAMPLE values in dataframe do not match those in test vcf"
         )
 
@@ -419,5 +193,8 @@ class TestFormatSample():
 
 if __name__ == "__main__":
     info = TestInfoColumn()
-    csq = TestCSQ()
-    TestFormatSample()
+    info.test_parsed_correct_columns_from_info_records()
+    info.test_parsed_correct_gnomAD_AF_values()
+
+    format = TestFormatSample()
+    format.test_format_sample_values_are_correct()
