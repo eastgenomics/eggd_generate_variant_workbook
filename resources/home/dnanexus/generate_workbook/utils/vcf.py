5a8d6356f935844c784332aa3602332ad5a08ae5
jethror1 <45037268+jethror1@users.noreply.github.com>
2022-04-28 14:21:33 +0100

    switch adding FORMAT column suffix to `_FMT`

--- resources/home/dnanexus/generate_workbook/utils/vcf.py
+++ resources/home/dnanexus/generate_workbook/utils/vcf.py
@@ -427,10 +427,19 @@ class vcf():
         str
             url string formatted as Excel hyperlink
         """
-        if not value[column] or pd.isna(value[column]) or \
-            value[column] == 'nan' or value[column] == '.':
-                # no value to build hyperlink
-                return value[column]
+        if (
+            not value[column] or pd.isna(value[column]) or
+            value[column] == 'nan' or value[column] == '.'
+        ):
+            # no value to build hyperlink
+            return value[column]
+
+        if (
+            column.lower() == 'existing_variation' and
+            not value[column].startswith('rs')
+        ):
+            # non-rsID in Existing_variation column => return without URL
+            return value[column]
 
         if 'gnomad' in column.lower():
             # handle gnomad differently as it requires chrom, pos, ref and
