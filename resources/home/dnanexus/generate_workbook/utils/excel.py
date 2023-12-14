96f0529424fc09533c9105f2ba037a06081061fb
wintun12 <winmin.tun@nhs.net>
2023-12-14 14:34:37 +0000

    more pep8 changes

--- resources/home/dnanexus/generate_workbook/utils/excel.py
+++ resources/home/dnanexus/generate_workbook/utils/excel.py
@@ -1401,13 +1401,14 @@ class excel():
         worksheet.merge_cells(
             start_row=6, end_row=6, start_column=6, end_column=10)
         worksheet.merge_cells(
-            start_row=7, end_row=7, start_column=6, end_column=10) 
+            start_row=7, end_row=7, start_column=6, end_column=10)
+        
     def drop_down(self) -> None:
         """
         function to add drop-downs in report and included sheets
-        in report sheet, drop-downs are added for the strength 
+        in report sheet, drop-downs are added for the strength
         columns and final classification
-        in included sheet, the 'Interpreted' column is added for 
+        in included sheet, the 'Interpreted' column is added for
         a YES/NO drop-down
         """
         wb = load_workbook(filename=self.args.output)
@@ -1415,13 +1416,13 @@ class excel():
         ws_re = wb['report']
         strength_options = '"Very Strong, Strong, Moderate, Supporting, NA"'
         strength_val = DataValidation(type='list', formula1=strength_options,
-                                    allow_blank=True)
+                                        allow_blank=True)
         strength_val.prompt = 'Select from the list'
         strength_val.promptTitle = 'Strength'
         ws_re.add_data_validation(strength_val)
         strength_val.add('H9:H24')
         cell_for_strenght = ['J8', 'J11', 'J12', 'J15', 'J16', 'J17', 'J20',
-                          'J21', 'J22', 'J23', 'J24', 'J25']
+                            'J21', 'J22', 'J23', 'J24', 'J25']
         for cell in cell_for_strenght:
             strength_val.add(cell)
         strength_val.showInputMessage = True
@@ -1431,12 +1432,14 @@ class excel():
         ws_re['B27'].font = Font(bold=True, name=DEFAULT_FONT.name)
         med_border = Border(left=MEDIUM, right=MEDIUM, bottom=MEDIUM,
                             top=MEDIUM)
-        ws_re['B27'].fill = PatternFill(patternType="solid", start_color="FFFF99")
+        ws_re['B27'].fill = PatternFill(patternType="solid",
+                                        start_color="FFFF99")
         ws_re['B27'].border = med_border
         ws_re['C27'].border = med_border
-        class_options='"Pathogenic,Likely Pathogenic,Uncertain Significance, Likely Benign, Benign"'
+        class_options = '"Pathogenic,Likely Pathogenic,Uncertain Significance,\
+                         Likely Benign, Benign"'
         class_val = DataValidation(type='list', formula1=class_options,
-                                    allow_blank=True)
+                                allow_blank=True)
         class_val.prompt = 'Select from the list'
         class_val.promptTitle = 'Variant Interpretation'
         ws_re.add_data_validation(class_val)
@@ -1449,18 +1452,18 @@ class excel():
         ws_in['AS1'].font = Font(name=DEFAULT_FONT.name)
         ws_in.column_dimensions['AS'].width = 12
         interpreted_options = '"YES,NO"'
-        data_val = DataValidation(type='list', formula1=interpreted_options, 
-                                allow_blank=True)
+        data_val = DataValidation(type='list', formula1=interpreted_options,
+                                    allow_blank=True)
         data_val.prompt = 'Choose YES or NO'
         data_val.promptTitle = 'Variant interpreted or not?'
         ws_in.add_data_validation(data_val)
-        num_variant=self.vcfs[0].shape[0]
-        for i in range (0,num_variant):
-            data_val.add(ws_in["AS"+str(i+2)])            
+        num_variant = self.vcfs[0].shape[0]
+        for i in range(0, num_variant):
+            data_val.add(ws_in["AS"+str(i+2)])
         data_val.showInputMessage = True
         data_val.showErrorMessage = True
         ws_in['AT1'] = 'Comment'
-        ws_in.column_dimensions['AT'].width =12
+        ws_in.column_dimensions['AT'].width = 12
         ws_in['AT1'].alignment = Alignment(wrapText=True, horizontal='center')
         ws_in['AT1'].border = THIN_BORDER
         wb.save(self.args.output)
