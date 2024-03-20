73e609788d30c18924e4fa63b2ce1732e6b6fa2f
AishaD <AishaDahir75@gmail.com>
2024-03-20 00:19:05 +0000

    IN-503 fix typo

--- resources/home/dnanexus/generate_workbook/generate_workbook.py
+++ resources/home/dnanexus/generate_workbook/generate_workbook.py
@@ -315,7 +315,7 @@ class arguments():
         parser.add_argument(
             '--AF', default='decimal', required=False,
             choices=['decimal', 'percent'],
-            help=('Presents the allele frequency (AF) as a decimale (0-1) '
+            help=('Presents the allele frequency (AF) as a decimal (0-1) '
             'or as a percent (0-100). Default is decimal.'
             )
         )
