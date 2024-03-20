94576eed41ab1810216228d5e709e26237809c5d
AishaD <AishaDahir75@gmail.com>
2024-03-20 12:55:05 +0000

    IN-503 indentation

--- resources/home/dnanexus/generate_workbook/utils/vcf.py
+++ resources/home/dnanexus/generate_workbook/utils/vcf.py
@@ -904,8 +904,8 @@ class vcf():
                 f"dbSNP: {x['CSQ_Existing_variation'] if x.get('CSQ_Existing_variation') else 'None'} \n"
                 f"dbSNP: {x['CSQ_Existing_variation'] if x.get('CSQ_Existing_variation') else 'None'} \n"
                 f"""Allele Frequency (VAF): {
-                    str(x['AF']) if x.get('AF') else 'None'
-                    }"""),
+                str(x['AF']) if x.get('AF') else 'None'
+                }"""),
             axis=1
         )
 
