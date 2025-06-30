f0d24d7a0b6e6440b7c376ae0210157ca6b4c985
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2025-06-30 15:51:36 +0100

    added tests

--- resources/home/dnanexus/generate_workbook/tests/test_excel.py
+++ resources/home/dnanexus/generate_workbook/tests/test_excel.py
@@ -23,28 +23,67 @@ def mocked_excel_file(mocker):
     mock_refs = mocker.Mock()
     return excel(mock_args, mock_vcfs, mock_additional_files, mock_refs)
 
-# lock_sheet()
-def test_attributes_are_set_correctly(mocked_excel_file):
+@pytest.fixture
+def locked_worksheet(mocked_excel_file):
     wb = openpyxl.Workbook()
     ws = wb.active
-    mocked_excel_file.lock_sheet(ws, password = "sheet_is_protected")
+    mocked_excel_file.lock_sheet(ws)
+    return ws
+
+@pytest.fixture
+def unlocked_worksheet():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    return ws
+
+# lock_sheet()
+def test_attributes_are_set_correctly(locked_worksheet):
     expected = {
-            "sheet": "1", 
-            "autoFilter": "0", 
-            "formatColumns": "0", 
-            "formatRows": "0", 
-            "formatCells": "0", 
-            "password": ws.protection.password
+            "sheet": "1",
+            "autoFilter": "0",
+            "formatColumns": "0",
+            "formatRows": "0",
+            "formatCells": "0",
+            "password": locked_worksheet.protection.password
         }
-    actual = dict(ws.protection)
+    actual = dict(locked_worksheet.protection)
     assert expected.items() <= actual.items()
 
-
 # unlock_specified_cells()
-def test_
+def test_unlock_specified_cells(mocked_excel_file, locked_worksheet):
+    target_cells = [locked_worksheet[cell] for cell in ["A1", "B2", "C3"]]
+    # ensure cells are currently locked
+    cells_are_locked = [cell.protection.locked for cell in target_cells]
+    assert all(cells_are_locked)
+    # unlock cells and ensure they are unlocked
+    mocked_excel_file.unlock_specified_cells(locked_worksheet, ["A1", "B2", "C3"])
+    cells_are_unlocked = [cell.protection.locked is False for cell in target_cells]
+    assert all(cells_are_unlocked)
+    # ensure unrelated cell remains unlocked
+    assert locked_worksheet["D4"].protection.locked
 
 # unlocked_region()
+def test_unlock_region(mocked_excel_file, locked_worksheet):
+    assert True
+
 # get_cells_in_columns()
+def test_get_cells_in_columns_returns_cells(mocked_excel_file, unlocked_worksheet):
+    # set header for unlocked worksheet
+    unlocked_worksheet["A1"].value = "COL_1"
+    unlocked_worksheet["B1"].value = "COL_2"
+    returned_cells = mocked_excel_file.get_cells_in_columns(
+            sheet=unlocked_worksheet, 
+            cols=["COL_1", "COL_2"], 
+            num_rows=2)
+    assert returned_cells == ["A2", "A3", "B2", "B3"]
+
+def test_get_cells_missing_column_error(mocked_excel_file, unlocked_worksheet):
+    with pytest.raises(RuntimeError, match=".*The column.*"):
+        mocked_excel_file.get_cells_in_columns(
+                sheet=unlocked_worksheet,
+                cols=["I_DONT_EXIST"],
+                num_rows=1)
+
 # store_list_in_sheet()
 # read_m_codes_file
 def test_success_upon_compliant_mcodes(mocked_excel_file, monkeypatch):
@@ -86,8 +125,6 @@ def test_str_to_drop_down_options_too_long(mocked_excel_file):
                 title="A",
                 sheet="A",
                 cells=["A1"])
-
-# write_variants()
 
 # format_cells_as_percentage
 def test_cells_formatted_as_perc(mocked_excel_file):
