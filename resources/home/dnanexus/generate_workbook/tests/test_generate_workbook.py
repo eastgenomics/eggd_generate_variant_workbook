acda765a8bbe299cfb7661b8a665452ad2ce2fce
growland2 <gregory.rowland@nhs.net>
2025-07-02 10:31:29 +0100

    unit tests for generate_workbook.py v2.11.0 changes

--- resources/home/dnanexus/generate_workbook/tests/test_generate_workbook.py
+++ resources/home/dnanexus/generate_workbook/tests/test_generate_workbook.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import pytest
+import argparse
 from unittest.mock import patch
-
 from generate_workbook import arguments
-
 
 class TestVerifyImages():
     """
@@ -106,3 +105,114 @@ class TestVerifyColours():
 
         with pytest.raises(AssertionError):
             self.args_obj.verify_colours()
+
+
+class TestDxFileID():
+    """
+    Tests for the generate_workbook.dx_file_id method.
+    Ensures that a DNAnexus file ID is correctly formatted and parsed.
+    """
+
+    # File IDs formatted as raw strings to prevent the hyphen "-" being
+    # interpreted by argsparse as a new argument
+    @pytest.mark.parametrize(
+        "dx_id",
+        ["notafileid", r"file-J0527G8487XjKgKJXjYqbgP",
+         r"file-J0527G8487XjKgKJXjYqbgPFX1",
+         r"file-J0527G8487XjKgKJXjYqbgPFfile-J0527G8487XjKgKJXjYqbgPF"]
+    )
+    def test_invalid_dx_file_id_raises_correct_error(self, dx_id):
+        """
+        Tests the correct error is raised when invalid DNAnexus file IDs are
+        passed to the --m_codes input
+        """
+        args_obj = object.__new__(arguments)
+        expected = "Invalid DNAnexus file ID:"
+
+        args = ['generate_workbook.py', '--m_codes', dx_id]
+        with patch("sys.argv", args):
+            # Complicated to test raising of argparse.ArgumentTypeError, see
+            # https://stackoverflow.com/a/49324489 for explanation
+            with pytest.raises(SystemExit) as e:
+                args_obj.args = args_obj.parse_args()
+
+        assert isinstance(e.value.__context__, argparse.ArgumentError)
+        assert expected in e.value.__context__.message
+
+    def test_valid_dx_file_id_is_returned(self):
+        """
+        Test valid DNAnexus file IDs are able to be parsed by argparse.
+        """
+        args_obj = object.__new__(arguments)
+
+        # File IDs formatted as raw strings to prevent the hyphen "-" being
+        # interpreted by argparse as a new argument
+        valid_dx_id = "file-J0527G8487XjKgKJXjYqbgPF"
+        args = ['generate_workbook.py', '--m_codes', valid_dx_id]
+
+        with patch("sys.argv", args):
+            args_obj.args = args_obj.parse_args()
+
+        assert args_obj.args.m_codes == "file-J0527G8487XjKgKJXjYqbgPF"
+
+
+class TestVerifySortBy():
+    """
+    Tests for the generate_workbook.verify_sort_by method.
+    Ensures that --sort_by input is correctly validated and parsed.
+    """
+
+    with patch("sys.argv", []):
+        args_obj = object.__new__(arguments)
+        args_obj.args = args_obj.parse_args()
+
+    def test_sort_by_input_without_colon_raises_error(self):
+        """
+        Test that a sort_by string without a colon raises a ValueError.
+        """
+        invalid_sort = "a_column_name 'True'"
+        self.args_obj.args.sort_by = invalid_sort
+        with pytest.raises(ValueError, match="Expected format"):
+            self.args_obj.verify_sort_by()
+
+    @pytest.mark.parametrize(
+        "sort", [["CHROM:"], [":True"]]
+    )
+    def test_sort_by_input_without_col_or_bool_raises_error(self, sort):
+        """
+        Test that sort_by values missing column name or boolean raise a
+        ValueError.
+        """
+        self.args_obj.args.sort_by = sort
+        with pytest.raises(ValueError, match="no column name or bool"):
+            self.args_obj.verify_sort_by()
+
+    @pytest.mark.parametrize(
+        "sort", [["CHROM:Not_a_bool"], ["CHROM:F"], ["CHROM:0"], ["CHROM:1"]]
+    )
+    def test_sort_by_input_with_invalid_bool_raises_error(self, sort):
+        """
+        Test that sort_by values with invalid booleans raise a ValueError.
+        """
+        self.args_obj.args.sort_by = sort
+        with pytest.raises(ValueError, match="Expected 'True'"):
+            self.args_obj.verify_sort_by()
+
+    @pytest.mark.parametrize(
+        "sort, expected",
+        [
+            (["CHROM:True"], {"CHROM": True}),
+            (["POS:False"], {"POS": False}),
+            (
+                ["REF:True", "POS:True", "CHROM:False"],
+                {"REF": True, "POS": True, "CHROM": False}
+            )
+        ]
+    )
+    def test_valid_sort_by_input_is_returned_correctly(self, sort, expected):
+        """
+        Test that valid sort_by input is correctly parsed into a dictionary.
+        """
+        self.args_obj.args.sort_by = sort
+        self.args_obj.verify_sort_by()
+        assert self.args_obj.args.sort_by == expected
